435530a24da72e6e91a8c68f0453142ad676fec7
wilmer.barcome <wilmer.barcome@amigo.edu.co>
2026-04-27 18:59:03 +0000

    Se corrige apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    id("com.google.gms.google-services")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -46,5 +47,6 @@ flutter {
 }
 
 dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
